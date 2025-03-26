2178db7936773e806e51bda7bb34e0598371da27
geunsu-son <gnsu0705@gmail.com>
2025-03-26 21:39:27 +0900

    Langchain Challenge Assignment 6

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -129,6 +129,9 @@ if file and openai_api_key:
             ("human", "{question}"),
         ]
     )
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+
     send_message("I'm ready! Ask away!", "ai", save=False)
     paint_history()
     message = st.chat_input("Ask anything about your file...")
