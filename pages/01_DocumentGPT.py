c8cce4d5561dd3530dda459a7965c154171f3bdf
geunsu-son <gnsu0705@gmail.com>
2025-03-26 21:35:31 +0900

    Langchain Challenge Assignment 6

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -31,7 +31,7 @@ class ChatCallbackHandler(BaseCallbackHandler):
         self.message_box.markdown(self.message)
 
 @st.cache_data(show_spinner="Embedding file...")
-def embed_file(file):
+def embed_file(file, openai_api_key):
     file_content = file.read()
 
     # 디렉토리 미리 생성
@@ -41,6 +41,7 @@ def embed_file(file):
     file_path = f"./.cache/files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
+
     cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
@@ -49,6 +50,7 @@ def embed_file(file):
     )
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
+    
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
